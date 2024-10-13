eab861a33f89fc57c96bf3272c02e272e32c25a6
ranjit1024 <ranjitdas2048@gmail.com>
2024-10-13 21:59:03 +0530

    adding into the database

--- Frontend/src/components/CreateTodo.jsx
+++ Frontend/src/components/CreateTodo.jsx
@@ -1,7 +1,30 @@
 export function CreateTodo(){
     return <div>
-        <input type="text" placeholder="Title" /><br />
-        <input type="text" placeholder="Descripition"/>
-        <button>Add a Todo</button>
+        <input type="text" placeholder="Title" style={{
+            padding:10,
+            margin:10,
+            border:"1px solid black",
+            borderRadius:"4px",
+            outline:"none"
+        }}/><br />
+
+        <input type="text" placeholder="Descripition" style={{
+            padding:10,
+            margin:10,
+            border:"1px solid black",
+            borderRadius:"4px",
+            outline:"none"
+        }}/><br />
+
+        <button style={{
+            padding:10,
+            margin:10,
+            border:"1px solid black",
+            borderRadius:"4px",
+            backgroundColor:"green",
+            color:"white",
+            cursor:"pointer"
+        }}>Add a Todo</button>
+        
     </div>
 }
---
+++ Frontend/src/components/Todos.jsx
@@ -0,0 +1,13 @@
+export function Todos(){
+    return <div>
+        <h1>Go to gym</h1>
+        <h2>You need to go to the gym</h2>
+        <button style={{
+            backgroundColor:"green",
+            padding:"10px",
+            borderRadius:"4px",
+            border:"1px solid black",
+            color:"white"
+        }}>Mark as Completed</button>
+    </div>
+}
