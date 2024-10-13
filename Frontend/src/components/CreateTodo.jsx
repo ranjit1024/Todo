77f80852d35b2d03205289a2a537a3f6e0a08139
ranjit1024 <ranjitdas2048@gmail.com>
2024-10-13 21:35:16 +0530

    Adding basic of frontend

---
+++ Frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,7 @@
+export function CreateTodo(){
+    return <div>
+        <input type="text" placeholder="Title" /><br />
+        <input type="text" placeholder="Descripition"/>
+        <button>Add a Todo</button>
+    </div>
+}
